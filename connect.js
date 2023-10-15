3e37811b6aeaee4a98ffac79f0432ddeaa2f2ba8
VanillaCrown <vanillacub23@gmail.com>
2023-10-15 16:03:50 -0500

    added js to connect and form validation

--- connect.js
+++ connect.js
@@ -1,58 +1,6 @@
-//javascript for image slider (about me section)
+// javascript for connect form (CONNECT PAGE) (validation)
 
-const images = document.querySelectorAll('#slider img');
-const previousImage = document.getElementById("prev");
-const nextImage = document.getElementById("next");
-
-let currentIndex = 0;
-
-
-function reset() {
-    for (let i = 0; i < images.length; i++) {
-      images[i].classList.remove('active');
-    }
-  }
-
-  function initializeSlider() {
-    reset();
-    images[currentIndex].classList.add('active');
-  }
-
-
-  function slideLeft() {
-    reset();
-    currentIndex--;
-    if (currentIndex < 0) {
-      currentIndex = images.length - 1;
-    }
-    images[currentIndex].classList.add('active');
-  }
-  
-  function slideRight() {
-    reset();
-    currentIndex++;
-    if (currentIndex >= images.length) {
-      currentIndex = 0;
-    }
-    images[currentIndex].classList.add('active');
-  }
-
-
-  initializeSlider();
-
-previousImage.addEventListener('click', function() {
-  slideLeft();
-});
-
-nextImage.addEventListener('click', function() {
-  slideRight();
-});
-
-
-
-// javascript for connect form (validation)
-
-const form = document.getElementById('exampleForm')
+const form = document.getElementById('connectForm')
 const submitButton = document.querySelector('.submit')
 const successMessage = document.getElementById('form-submitted-msg')
 
@@ -90,16 +38,6 @@ const handleChange = () => {
       element.nextElementSibling.style.color = '#CED4DA'
       element.nextElementSibling.style.display = 'none'
       element.previousElementSibling.style.color = '#212529'
-    }
-
-    // If the element is a select dropdown and the default option is selected, style it with a red border and red text
-    if (element.nodeName === 'SELECT'
-          && element.value === 'Please select an option'
-    ) {
-      element.style.borderColor = 'red'
-      element.nextElementSibling.style.color = 'red'
-      element.nextElementSibling.style.display = 'block'
-      element.previousElementSibling.style.color = 'red'
     }
   })
 
